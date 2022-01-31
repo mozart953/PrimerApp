28a4fce4f9b2ecde2e87cf178dc2ccf0b663c0f3
Wilson Pop <wilsonnianopop@gmail.com>
2022-01-30 18:01:04 -0600

    subida de archivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Route} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ComponentedosComponent } from './componentedos/componentedos.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+
+import {DataService} from './data.service';
+import { AboutComponent } from './about/about.component';
+
+const Routes: Route[] = [
+  {path: '', component : ComponentedosComponent },
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentedosComponent,
+    UsuariosComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(Routes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
